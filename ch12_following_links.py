0f3302c2b55fd59219b42be42353dd29c2ab5cbf
Sam Byrne <ssbyrne@uw.edu>
2023-03-22 09:15:25 -0700

    following links is complete

--- ch12_following_links.py
+++ ch12_following_links.py
@@ -1,25 +1,22 @@
 from urllib.request import urlopen
-from bs4 import BeautifulSoup
 import ssl
+from bs4 import BeautifulSoup
 
 # ignore cert errors
 ctx = ssl.create_default_context()
 ctx.check_hostname  = False
 ctx.verify_mode = ssl.CERT_NONE
 
-# take user args
-url = input("Enter URL: ")
-if len(url) == 0:
-	url = 'http://py4e-data.dr-chuck.net/known_by_Fikret.html'
-	# url = 'http://py4e-data.dr-chuck.net/known_by_Lillyann.html'
+# parameters
 
-name_position = input("Follow which link position on page: ")
-if len(name_position) == 0:
-	name_position = 3
-
-link_depth = input("Follow links x times: ")
-if len(link_depth) == 0:
-	link_depth = 4
+# drop this line in to get into debugger
+# import pdb; pdb.set_trace()
+# url = 'http://py4e-data.dr-chuck.net/known_by_Fikret.html'
+# name_position = 3
+# link_depth = 4
+url = 'http://py4e-data.dr-chuck.net/known_by_Lillyann.html'
+name_position = 18
+link_depth = 7
 
 # start counter
 link_count = 0
@@ -27,17 +24,40 @@ link_count = 0
 # open url handle
 html = urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
-tags = soup('a') 
-
+tags = soup('a')
 
 loop_count = 0
 
-for tag in tags:
-	loop_count += 1
-	print(tag.contents)
-	print(tag)
+while True:
+    print(url)
+    if loop_count >= link_depth:
+        print(f"breaking after {loop_count} cycles - exceeded limit")
+        break
+    else:
+        # open url an dget tags
+        html = urlopen(url, context=ctx).read()
+        soup = BeautifulSoup(html, 'html.parser')  
+        tag = soup('a')[name_position - 1]
+        # print(tag.get('href', None))
+        url = tag.get('href', None)
+        loop_count += 1
+
+
+# exploration - how to extract information
+# for tag in tags:
+#     loop_count += 1
+#     print(tag)
+#     print(tag.get('href', None))
+#     print(tag.contents[0])
+#     print(tag.attrs)
+#     print(tag.attrs['href'])
+
+# for tag in tags:
+#     loop_count += 1
+#     if loop_count == name_position:
+#         url = tag.get('href', None)
+#         html = urlopen(url, context=ctx).read()
+#         soup = BeautifulSoup(html, 'html.parser')
+#         tags = soup('a')
 
 print('done')
-
-
-
---
+++ url = input("Enter URL - ").py
@@ -0,0 +1,3 @@
+url = input("Enter URL - ")
+if len(url) == 0:
+    url = 'http://py4e-data.dr-chuck.net/known_by_Fikret.html'
