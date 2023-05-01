59b42f0466d4d5bff0c75ec6bcf24838294d31b2
Sam Byrne <ssbyrne@uw.edu>
2023-05-01 08:23:39 -0700

    added worked xml json examples

---
+++ py4e_worked_examples/json1.py
@@ -0,0 +1,22 @@
+import json
+
+data = '''
+{
+  "name" : "Chuck",
+  "phone" : {
+    "type" : "intl",
+    "number" : "+1 734 303 4456"
+   },
+   "email" : {
+     "hide" : "yes"
+   }
+}'''
+
+info = json.loads(data)
+
+# print(data)
+# print(info)
+# print(info["phone"]["number"])
+
+print('Name:', info["name"])
+print('Hide:', info["email"]["hide"])
